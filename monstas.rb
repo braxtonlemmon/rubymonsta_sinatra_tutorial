48e80ff47d850b911bb5521a90d9bb937c33e34f
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-06-12 12:30:38 +0200

    Store and read data

--- monstas.rb
+++ monstas.rb
@@ -1,7 +1,20 @@
 require "sinatra"
 require "erb"
 
+def store_name(filename, string)
+	File.open(filename, "a+") do |file|
+		file.puts(string)
+	end
+end
+
+def read_names
+	return [] unless File.exist?("names.txt")
+	File.read("names.txt").split("\n")
+end
+
 get "/monstas" do
 	@name = params["name"]
+	@names = read_names
+	store_name("names.txt", @name)
 	erb :monstas
 end
